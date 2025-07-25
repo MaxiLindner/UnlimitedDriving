e1115fba62ead156f6855129fc78cd4a096170c6
Maxi <mali-wahnbek@web.de>
2025-07-25 22:09:33 +0200

    index.html test

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Fügen Sie diese Zeile hinzu
+  base: '/UnlimitedDriving/',
   build: {
     rollupOptions: {
       output: {
